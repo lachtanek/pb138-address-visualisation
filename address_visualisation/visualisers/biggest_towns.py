5e3f81c9a09dbbffe746542d62a02e98282a0ac5
Jan Tojnar <jtojnar@gmail.com>
2016-06-10 22:21:15 +0200

    rewrite biggest_towns visualiser

--- address_visualisation/visualisers/biggest_towns.py
+++ address_visualisation/visualisers/biggest_towns.py
@@ -34,23 +34,32 @@ class BiggestTownsVisualiser(Visualiser):
 
 		"""
 		root = self.db_tree.getroot()
+
 		kraje = root.findall(".//Kraj")
-		maxValues = [None]*len(kraje)
-		i = 0
-		for kraj in kraje:
-			maximum = (0, None, None, kraj.find("Nazev").text)
-			for okres in root.iter('Okres'):
-				if okres.get("kod")[0:2] == kraj.get("kod"):
-					for obec in root.iter('Obec'):
-						if obec.get("okres") == okres.get("kod"):
-							for obec_ulice in root.iter('AdresniMista'):
-								if obec_ulice.get("obec") == obec.get("kod"):
-									pocet_adresnich_mist = int(obec_ulice.find("pocetAdresnichMist").text)
-									if pocet_adresnich_mist > maximum[0]:
-										maximum = (pocet_adresnich_mist, obec.get("kod"), obec.find("Nazev").text, kraj.find("Nazev").text)
-			maxValues[i] = maximum
-			i = i + 1
-		return maxValues
+		max_values = {kraj.get("kod"): (0, None, None, kraj.find("Nazev").text) for kraj in kraje}
+
+		obce = {
+			obec.get("kod"): (
+				int(obec.find("PocetAdresnichMist").text) if obec.find("PocetAdresnichMist") else 0,
+				obec.find("Nazev").text,
+				obec.get("okres")[0:2]
+			) for obec in root.iter('Obec')
+		}
+
+		for ulice in root.iter("Ulice"):
+			kod_obce = ulice.get("obec")
+			pocet_adresnich_mist = int(ulice.find("PocetAdresnichMist").text)
+			obce[kod_obce] = (obce[kod_obce][0] + pocet_adresnich_mist, obce[kod_obce][1], obce[kod_obce][2])
+
+		for kod_obce, obec in obce.items():
+			pocet_adresnich_mist = obec[0]
+			nazev_obce = obec[1]
+			kod_kraje = obec[2]
+			if pocet_adresnich_mist > max_values[kod_kraje][0]:
+				nazev_kraje = max_values[kod_kraje][3]
+				max_values[kod_kraje] = (pocet_adresnich_mist, kod_obce, nazev_obce, nazev_kraje)
+
+		return max_values
 
 	def run(self):
 		"""
