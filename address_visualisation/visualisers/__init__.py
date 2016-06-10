3bad6f8373677e5b305268ecf51de37daa388114
Jan Tojnar <jtojnar@gmail.com>
2016-06-10 19:21:18 +0200

    remove java visualiser

--- address_visualisation/visualisers/__init__.py
+++ address_visualisation/visualisers/__init__.py
@@ -1,7 +1,6 @@
 from .sample_streets import SampleStreetsVisualiser
 from .biggest_towns import BiggestTownsVisualiser
 from .extreme_street_names import ExtremeStreetNamesVisualiser
-# from .java_visualiser import JavaVisualiser
 from .longest_streets import LongestStreetsVisualiser
 from .square_count import SquareCountVisualiser
 from .town_with_most_streets import TownWithMostStreetsVisualiser
