b0a242d5cd4d95ee7b1cd580e604833031825aef
A.Nohejlova <A.Nohejlova@ANohejlova-HP>
2016-06-11 17:32:41 +0200

    oprava v transform u areas

--- address_visualisation/transformToFeatureCollection.py
+++ address_visualisation/transformToFeatureCollection.py
@@ -149,18 +149,18 @@ def feature_collection_from_areas(values, country_tree, collection_title):
 		FeatureCollection containing Polygons made from information about areas from `values`.
 	"""
 	areas_collection = []
-	sorted_values = max(values, key=lambda area: area[Area.measured])
-	opacity = 0
+	max_value = max(values, key=lambda area: area[Area.measured])
+#	opacity = 0
 	for area in values:
 		area_positions = country_tree.getroot().findall(".//Okres[@kod='"+area[Area.code]+"']/Geometrie/PosList")
 		boundaries = parse_street_lines(area_positions)
 		
 		polygon = Polygon(boundaries)
-		style = {'fill': 'rgba(240, 0, 0, ' + str(get_opacity(area[Area.measured], sorted_values[0][Area.measured])) + ')'}
+		style = {'fill': 'rgba(240, 0, 0, ' + str(get_opacity(area[Area.measured], max_value) + ')'}
 		properties = {'name': area[Area.area_name], 'measured': area[Area.measured], 'style': style}
 
 		area_feature = Feature(geometry=polygon, properties=properties, id=int(area[Area.code]))
 		areas_collection.append(area_feature)
-		opacity = opacity + (1 / len(sorted_values))
+#		opacity = opacity + (1 / len(sorted_values))
 
 	return FeatureCollection(collection_title, areas_collection)
