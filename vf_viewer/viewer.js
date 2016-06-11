63d6707bd45f57fe835d26e47e6c10c712779957
Jan Tojnar <jtojnar@gmail.com>
2016-06-11 22:04:35 +0200

    Show visualisation name in the title

--- vf_viewer/viewer.js
+++ vf_viewer/viewer.js
@@ -99,8 +99,11 @@ window.onload = function() {
 		window.location.search = '';
 	} else {
 		const infoBar = createInfoBar(fileName);
+
+		document.title = 'Address visualisation – ' + visualisations.get(fileName).name;
 		document.body.classList.remove('front');
 		sideBar.collapse();
+
 		proj4.defs('urn:ogc:def:crs:EPSG::5514','+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=589,76,480,0,0,0,0 +units=m +no_defs');
 
 		function styleFunction(feature) {
