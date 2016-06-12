3209e1c7304751039f2b608762ebfed3130da31d
A.Nohejlova <A.Nohejlova@ANohejlova-HP>
2016-06-12 15:37:39 +0200

    pridana dokumentace init.py souboru, uprava podle pylint pro helpers a visualise_registry, uprava barvy okresu v transform_to_feature_collection

--- address_visualisation/visualisers/__init__.py
+++ address_visualisation/visualisers/__init__.py
@@ -1,3 +1,4 @@
+"""Package which contains instances of Visualiser."""
 from .profane_streets import ProfaneStreetsVisualiser
 from .biggest_towns import BiggestTownsVisualiser
 from .extreme_street_names import ExtremeStreetNamesVisualiser
@@ -6,10 +7,10 @@ from .square_count import SquareCountVisualiser
 from .town_with_most_streets import TownWithMostStreetsVisualiser
 
 DEFAULT_VISUALISERS = [
-	(ProfaneStreetsVisualiser, 'profane_streets'),
-	(BiggestTownsVisualiser, 'biggest_towns'),
-	(ExtremeStreetNamesVisualiser, 'extreme_street_names'),
-	(LongestStreetsVisualiser, 'longest_streets'),
-	(SquareCountVisualiser, 'square_count'),
-	(TownWithMostStreetsVisualiser, 'town_with_most_streets')
-]
+				(ProfaneStreetsVisualiser, 'profane_streets'),
+				(BiggestTownsVisualiser, 'biggest_towns'),
+				(ExtremeStreetNamesVisualiser, 'extreme_street_names'),
+				(LongestStreetsVisualiser, 'longest_streets'),
+				(SquareCountVisualiser, 'square_count'),
+				(TownWithMostStreetsVisualiser, 'town_with_most_streets')
+				]
