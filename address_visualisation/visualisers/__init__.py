ecd087e811f35ab9044daeecefdeb773747198a4
A.Nohejlova <A.Nohejlova@ANohejlova-HP>
2016-06-12 12:43:54 +0200

    transform_to_feature_collection - oprava stylu

--- address_visualisation/visualisers/__init__.py
+++ address_visualisation/visualisers/__init__.py
@@ -1,17 +1,15 @@
-from .sample_streets import SampleStreetsVisualiser
+from .profane_streets import ProfaneStreetsVisualiser
 from .biggest_towns import BiggestTownsVisualiser
 from .extreme_street_names import ExtremeStreetNamesVisualiser
 from .longest_streets import LongestStreetsVisualiser
-from .profane_streets import ProfaneStreetsVisualiser
 from .square_count import SquareCountVisualiser
 from .town_with_most_streets import TownWithMostStreetsVisualiser
 
 DEFAULT_VISUALISERS = [
-	(SampleStreetsVisualiser, 'sample_streets'),
+	(ProfaneStreetsVisualiser, 'profane_streets'),
 	(BiggestTownsVisualiser, 'biggest_towns'),
 	(ExtremeStreetNamesVisualiser, 'extreme_street_names'),
 	(LongestStreetsVisualiser, 'longest_streets'),
-	(ProfaneStreetsVisualiser, 'profane_streets'),
 	(SquareCountVisualiser, 'square_count'),
 	(TownWithMostStreetsVisualiser, 'town_with_most_streets')
 ]
