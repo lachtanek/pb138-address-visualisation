3df633dcd1e66c09abdd924b8adf9d052a7ce56b
Jan Tojnar <jtojnar@gmail.com>
2016-06-13 16:20:43 +0200

    add makefile for docbook

---
+++ docbook/replace_xinclude.hh
@@ -0,0 +1,7 @@
+<?php
+// This is Hack script, not a PHP script. Any resemblance is purely coincidental.
+$d = file_get_contents('docbook.xml');
+$d = preg_replace_callback('~<xi:include href="([^"]+)" />~', function($m) {
+	return file_get_contents($m[1]);
+}, $d);
+file_put_contents('docbook.tmp.xml', $d);
