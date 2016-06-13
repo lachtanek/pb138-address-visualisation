de97621d8438800b1086efa68b17078791e87dcc
Jan Tojnar <jtojnar@gmail.com>
2016-06-14 01:22:35 +0200

    improve typography in docbook

--- docbook/replace_xinclude.hh
+++ docbook/replace_xinclude.hh
@@ -1,5 +1,5 @@
 <?php
-// This is Hack script, not a PHP script. Any resemblance is purely coincidental.
+// This is a Hack script, not a PHP script. Any resemblance is purely coincidental.
 $d = file_get_contents('docbook.xml');
 $d = preg_replace_callback('~<xi:include href="([^"]+)" />~', function($m) {
 	return file_get_contents($m[1]);
