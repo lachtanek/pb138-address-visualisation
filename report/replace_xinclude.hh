3c641e40ac2d6103d09281e29505a07a2a6a3181
Jan Tojnar <jtojnar@gmail.com>
2016-06-14 02:25:41 +0200

    rename docbook to report

--- report/replace_xinclude.hh
+++ report/replace_xinclude.hh
@@ -1,7 +1,7 @@
 <?php
 // This is a Hack script, not a PHP script. Any resemblance is purely coincidental.
-$d = file_get_contents('docbook.xml');
+$d = file_get_contents('report.xml');
 $d = preg_replace_callback('~<xi:include href="([^"]+)" />~', function($m) {
 	return file_get_contents($m[1]);
 }, $d);
-file_put_contents('docbook.tmp.xml', $d);
+file_put_contents('report.tmp.xml', $d);
