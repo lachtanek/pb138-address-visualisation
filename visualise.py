8abcc4467cea0a03ea02077fb5e1b50e948d5f2f
Jan Tojnar <jtojnar@gmail.com>
2016-06-14 10:16:11 +0200

    allow to specify which visualisers are to be run

--- visualise.py
+++ visualise.py
@@ -5,6 +5,7 @@ Runs logging and visualiserRegistry for available visualisers.
 import address_visualisation as AV
 from address_visualisation.visualisers import ProfaneStreetsVisualiser, BiggestTownsVisualiser, ExtremeStreetNamesVisualiser, LongestStreetsVisualiser, SquareCountVisualiser, TownWithMostStreetsVisualiser
 import logging
+import sys
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
@@ -17,7 +18,21 @@ DEFAULT_VISUALISERS = [
 	(TownWithMostStreetsVisualiser, 'town_with_most_streets')
 ]
 
-visualisers = DEFAULT_VISUALISERS
+visualisers = []
+if len(sys.argv) > 1:
+	for a in sys.argv[1:]:
+		s = a.split('=', 1)
+		if len(s) != 2:
+			raise Exception('Argument has to be in the form XxxVisualiser=output_name.')
+		v = globals()[s[0]]
+		if not issubclass(v, AV.Visualiser):
+			raise Exception('The class name has to be an instance of visualiser.')
+		if s[1].find('/') != -1:
+			raise Exception('The output file name cannot contain a forward slash.')
+
+		visualisers.append((v, s[1]))
+else:
+	visualisers = DEFAULT_VISUALISERS
 
 visreg = AV.VisualiserRegistry('resources/db.xml')
 visreg.register_visualiser_set(visualisers)
